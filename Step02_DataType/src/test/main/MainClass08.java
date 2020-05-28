f242288cc30d842ec2434d1c55f6910b45d04259
acorn <zip7613@gmail.com>
2020-05-28 09:55:47 +0900

    MainClass08.java 수정

--- Step02_DataType/src/test/main/MainClass08.java
+++ Step02_DataType/src/test/main/MainClass08.java
@@ -3,8 +3,9 @@ package test.main;
 public class MainClass08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Git test");
+		System.out.println("변경1");
+		System.out.println("변경2");
 	}
 
 }
